d8c938861c8d9bb2bd9945b45e0bb93a8ad8c997
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 01:35:59 -0700

    Update build_mac.sh

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+set -e
+git clone --depth 1 https://github.com/steinbergmedia/vst3sdk external/vst3sdk
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DVST3_SDK_DIR=$(pwd)/external/vst3sdk
+cmake --build build --config Release --target ALL_BUILD --parallel
