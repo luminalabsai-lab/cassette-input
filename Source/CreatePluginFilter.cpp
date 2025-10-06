39dbace132beab5c924f2e0c0f1e3821dce04dd8
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 15:33:53 -0700

    Update CreatePluginFilter.cpp

--- Source/CreatePluginFilter.cpp
+++ Source/CreatePluginFilter.cpp
@@ -1,7 +1,7 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-// C linkage so AU/VST3 wrappers can find the unmangled symbol
+// C linkage + export so AU/VST3 wrappers can find the factory
 extern "C" JUCE_API juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new CassetteInputAudioProcessor();
