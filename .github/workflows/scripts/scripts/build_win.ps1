edb846b6f9945496ea20e42243c6b98e2ccee783
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 01:02:51 -0700

    Delete .github/workflows directory

--- .github/workflows/scripts/scripts/build_win.ps1
+++
@@ -1,3 +0,0 @@
-git clone --depth 1 https://github.com/steinbergmedia/vst3sdk external/vst3sdk
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -A x64 -DVST3_SDK_DIR=$pwd\\external\\vst3sdk
-cmake --build build --config Release --target ALL_BUILD -- /m
