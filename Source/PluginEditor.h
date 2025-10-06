c77a34eccc23dc587f6d1a271a56105a7a4b9b86
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 02:52:43 -0700

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <JuceHeader.h>
-class CassetteInputAudioProcessor; // fwd
+#include "PluginProcessor.h" // or forward declare the class
 
-class CassetteInputAudioProcessorEditor : public juce::AudioProcessorEditor {
+class CassetteInputAudioProcessorEditor : public juce::AudioProcessorEditor
+{
 public:
-  explicit CassetteInputAudioProcessorEditor (CassetteInputAudioProcessor& p);
-  ~CassetteInputAudioProcessorEditor() override = default;
+    explicit CassetteInputAudioProcessorEditor (CassetteInputAudioProcessor& p);
+    ~CassetteInputAudioProcessorEditor() override = default;
 
-  void paint (juce::Graphics&) override;
-  void resized() override;
+    void paint (juce::Graphics&) override;
+    void resized() override;
 
 private:
-  CassetteInputAudioProcessor& processor; // keep a ref
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CassetteInputAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CassetteInputAudioProcessorEditor)
 };
