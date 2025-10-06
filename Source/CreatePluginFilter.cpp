f0a8a3b358bb3293a26170c8e44c272856fe6216
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 15:13:07 -0700

    Delete Source/CreatePluginFilter.cpp

--- Source/CreatePluginFilter.cpp
+++
@@ -1,8 +0,0 @@
-#include <JuceHeader.h>
-#include "PluginProcessor.h"
-
-// This must be C linkage + exported so AU/VST3 wrappers can find it.
-extern "C" JUCE_API juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new CassetteInputAudioProcessor();
-}
