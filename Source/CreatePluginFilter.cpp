ded205e5ba192c8f39c9f83bfb08f890e3b4d167
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 04:03:12 -0700

    Delete Source/CreatePluginFilter.cpp

--- Source/CreatePluginFilter.cpp
+++
@@ -1,8 +0,0 @@
-#include <JuceHeader.h>
-#include "PluginProcessor.h"
-
-// Single, global factory required by JUCE plugin clients
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new CassetteInputAudioProcessor();
-}
