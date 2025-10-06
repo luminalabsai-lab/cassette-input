8f2266b2d8ebb4bbd2e2d57b55302e42833aff57
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 02:55:15 -0700

    Update Parameters.h

--- Source/Parameters.h
+++ Source/Parameters.h
@@ -1,9 +1,5 @@
 #pragma once
 #include <JuceHeader.h>
 
-inline juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
-{
-  // Empty for now; we’ll add real parameters once it builds cleanly
-  std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
-  return { params.begin(), params.end() };
-}
+// Declaration only; implementation lives in Parameters.cpp
+juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
