3c792dd4166dcc8abc58877ed6325b71214ec998
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 01:54:13 -0700

    Update build_mac.sh

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-echo "Starting build script"
-git clone --depth 1 https://github.com/steinbergmedia/vst3sdk external/vst3sdk || echo "VST3SDK already exists"
+echo "Starting build_mac.sh"
+git clone --depth 1 https://github.com/steinbergmedia/vst3sdk external/vst3sdk || echo "vst3sdk already exists"
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DVST3_SDK_DIR=$(pwd)/external/vst3sdk
 cmake --build build --config Release --target ALL_BUILD --parallel
-echo "Build script finished"
+echo "build_mac.sh finished"
