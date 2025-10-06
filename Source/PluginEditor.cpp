9d8b661b9069929c8fcb5aece84929afbbee2e49
luminalabsai-lab <luminalabsai@gmail.com>
2025-10-06 02:42:38 -0700

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,15 +1,17 @@
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
 
 CassetteInputAudioProcessorEditor::CassetteInputAudioProcessorEditor (CassetteInputAudioProcessor& p)
-: AudioProcessorEditor (&p) {
-    setSize (600, 400);
+: juce::AudioProcessorEditor (&p), processor (p)
+{
+  setSize (600, 400);
 }
 
-CassetteInputAudioProcessorEditor::~CassetteInputAudioProcessorEditor() {}
-
-void CassetteInputAudioProcessorEditor::paint (juce::Graphics& g) {
-    g.fillAll (juce::Colours::black);
-    // Draw cassette window stub, meters, big knobs...
+void CassetteInputAudioProcessorEditor::paint (juce::Graphics& g)
+{
+  g.fillAll (juce::Colours::black);
+  g.setColour (juce::Colours::white);
+  g.drawFittedText ("Cassette Input (stub UI)", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void CassetteInputAudioProcessorEditor::resized() {}
